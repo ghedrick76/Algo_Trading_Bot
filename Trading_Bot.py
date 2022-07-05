2ab08fe6b58ba4e60a3326ca832b218809e12e91
ghedrick76 <graysonh76@icloud.com>
2022-07-05 16:36:58 -0400

    connected to boto3

---
+++ Trading_Bot.py
@@ -0,0 +1,13 @@
+import boto3
+
+# Use Amazon S3
+s3 = boto3.resource('s3')
+
+# Print out bucket names
+for bucket in s3.buckets.all():
+    print(bucket.name)
+
+# Upload a new file
+data = open('credentials.txt', 'rb')
+s3.Bucket('algo-trading-bot').put_object(Key='credentials.txt', Body=data)
+
